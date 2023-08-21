b817aa2935f5d4858c70ba248e872ed9330c4f83
Carl Cervone <42869436+ccerv1@users.noreply.github.com>
2023-08-21 15:58:14 -0500

    fix: add query

--- queries/leaderboard.sql
+++ queries/leaderboard.sql
@@ -1,1 +1,11 @@
-
+-- Shows the top contributors by USD value of votes cast 
+-- as well as the number of unique projects/addresses and rounds they have voted on.
+SELECT
+    "voter",
+    SUM("amountUSD") AS "sumUSD",
+    COUNT(DISTINCT "projectId") AS "uniqueProjects",
+    COUNT(DISTINCT "grantAddress") AS "uniqueGrantAddresses",
+    COUNT(DISTINCT "roundId") AS "uniqueRounds"
+FROM public."Vote"
+GROUP BY "voter"
+ORDER BY "sumUSD" DESC;
