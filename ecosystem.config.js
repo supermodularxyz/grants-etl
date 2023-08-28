94f225c97fa61a30d9236f68cf389452e909df80
Jedi Maker <meremezeukah@gmail.com>
2023-08-27 21:59:26 -0500

    pm2 setup

---
+++ ecosystem.config.js
@@ -0,0 +1,19 @@
+module.exports = {
+  apps: [
+    {
+      name: 'etl:1',
+      script: 'yarn',
+      args: 'run etl:1',
+    },
+    {
+      name: 'etl:10',
+      script: 'yarn',
+      args: 'run etl:10',
+    },
+    {
+      name: 'etl:424',
+      script: 'yarn',
+      args: 'run etl:424',
+    },
+  ],
+}
