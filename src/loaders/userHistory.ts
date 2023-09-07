85221d084637aa41b07784d25090c692eed6960e
Jedi Maker <meremezeukah@gmail.com>
2023-09-06 22:50:44 -0500

    update dune history loader

--- src/loaders/userHistory.ts
+++ src/loaders/userHistory.ts
@@ -18,30 +18,39 @@ type Row = {
   chain: string[]
 }
 
-const client = new DuneClient(process.env.DUNE_API_KEY ?? '')
-
 const managerUserHistory = async ({ chainId, prisma }: Props): Promise<any> => {
   // load all users count
   const userCount = await prisma.user.count()
+
+  console.log(`Indexing ${userCount} users`)
 
   if (userCount === 0) {
     console.log(`No users found`)
     return
   }
 
-  let currentUser = 0
+  let cursor
 
   do {
-    currentUser += 1
-
     // pick off users based on id and increment till end of list
-    const user = await prisma.user.findUnique({
-      where: {
-        id: currentUser,
+    const users: { id: number; address: string }[] = await prisma.user.findMany({
+      take: 1000,
+      orderBy: {
+        id: 'asc',
       },
+      ...(cursor && { skip: 1, cursor: { id: cursor } }),
     })
 
-    const duneRes = await client.refresh(2974152, [QueryParameter.text('wallet_list', user?.address as string)])
+    if (users.length === 0) {
+      break
+    }
+
+    cursor = users[users.length - 1].id
+
+    const userAddresses = users.map((u) => u.address).join(', ')
+
+    const client = new DuneClient(process.env.DUNE_API_KEY ?? '')
+    const duneRes = await client.refresh(2974152, [QueryParameter.text('wallet_list', userAddresses)])
 
     const rows = (duneRes.result?.rows || []) as Row[]
 
@@ -53,24 +62,34 @@ const managerUserHistory = async ({ chainId, prisma }: Props): Promise<any> => {
           data.push({
             wallet: row.wallet,
             direction: row.direction,
-            block_number: row.block_number[i],
+            block_number: row.block_number[i] || 0,
             block_time: new Date(row.block_time[i]).toISOString(),
-            tx_hash: row.tx_hash[i],
-            neighbor_address: row.neighbor_address[i],
+            tx_hash: row.tx_hash[i] || '',
+            neighbor_address: row.neighbor_address[i] || '',
             symbol: row.symbol[i] || '',
             token_amount: row.token_amount[i] || 0,
-            chain: row.chain[i],
+            chain: row.chain[i] || '',
           })
         }
       }
     }
 
-    await prisma.userHistory.createMany({
-      data,
-    })
+    const chunkSize = 300
+    const chunks = []
 
-    console.log(`Completed user history ${currentUser} / ${userCount}`)
-  } while (currentUser < userCount)
+    for (let i = 0; i < data.length; i += chunkSize) {
+      const chunk = data.slice(i, i + chunkSize)
+      chunks.push(chunk)
+    }
+
+    for (const chunk of chunks) {
+      await prisma.userHistory.createMany({
+        data: chunk,
+      })
+    }
+
+    console.log(`At user index ${cursor}`)
+  } while ((cursor as number) > 0)
 }
 
 export default managerUserHistory
