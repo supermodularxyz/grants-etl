5aa9861cf279b16aff46bdfe6a3d54f5be5d820d
Jedi Maker <meremezeukah@gmail.com>
2023-09-11 23:15:05 -0500

    add retry redash api if server error

--- src/loaders/userStamps.ts
+++ src/loaders/userStamps.ts
@@ -39,81 +39,85 @@ const managerUserStamps = async ({ chainId, prisma }: Props): Promise<any> => {
   let cursor
 
   do {
-    // pick off users based on id and increment till end of list
-    const users: { id: number; address: string }[] = await prisma.user.findMany({
-      take: 20,
-      orderBy: {
-        id: 'asc',
-      },
-      ...(cursor && { skip: 1, cursor: { id: cursor } }),
-    })
+    try {
+      // pick off users based on id and increment till end of list
+      const users: { id: number; address: string }[] = await prisma.user.findMany({
+        take: 20,
+        orderBy: {
+          id: 'asc',
+        },
+        ...(cursor && { skip: 1, cursor: { id: cursor } }),
+      })
 
-    if (users.length === 0) {
-      break
-    }
+      if (users.length === 0) {
+        break
+      }
 
-    cursor = users[users.length - 1].id
+      const userAddresses = users.map((u) => `'${u.address.toLowerCase()}'`).join(',')
 
-    const userAddresses = users.map((u) => `'${u.address.toLowerCase()}'`).join(',')
+      const refreshUrl = `${stampUrl}/api/queries/${queryId}/results`
 
-    const refreshUrl = `${stampUrl}/api/queries/${queryId}/results`
+      const refreshResponse = await axios.post(
+        refreshUrl,
+        { parameters: { wallet_list: userAddresses }, max_age: 0 },
+        headers
+      )
 
-    const refreshResponse = await axios.post(
-      refreshUrl,
-      { parameters: { wallet_list: userAddresses }, max_age: 0 },
-      headers
-    )
+      const jobId = refreshResponse.data.job.id
 
-    const jobId = refreshResponse.data.job.id
+      let jobStatus = 1
+      let jobData
 
-    let jobStatus = 1
-    let jobData
+      do {
+        const jobUrl = `${stampUrl}/api/jobs/${jobId}`
 
-    do {
-      const jobUrl = `${stampUrl}/api/jobs/${jobId}`
+        const jobResponse = await axios.get(jobUrl, headers)
+
+        jobData = jobResponse.data
+        jobStatus = jobResponse.data.job.status
+
+        if (jobStatus === 3) {
+          console.log('Query refresh completed!')
+          break
+        }
+
+        if (jobStatus === 4) {
+          console.log('Query refresh failed!')
+          break
+        }
+
+        console.log(`5 seconds wait before next check for job completion`)
+        await new Promise((resolve) => setTimeout(resolve, 5000))
+      } while (jobStatus < 3)
+
+      const jobUrl = `${stampUrl}/api/queries/${queryId}/results/${jobData.job.query_result_id}.json`
 
       const jobResponse = await axios.get(jobUrl, headers)
 
-      jobData = jobResponse.data
-      jobStatus = jobResponse.data.job.status
+      const rows = jobResponse.data.query_result.data.rows as Row[]
 
-      if (jobStatus === 3) {
-        console.log('Query refresh completed!')
-        break
+      const chunkSize = 300
+      const chunks = []
+
+      for (let i = 0; i < rows.length; i += chunkSize) {
+        const chunk = rows.slice(i, i + chunkSize)
+        chunks.push(chunk)
       }
 
-      if (jobStatus === 4) {
-        console.log('Query refresh failed!')
-        break
+      for (const chunk of chunks) {
+        await prisma.stamp.createMany({
+          data: chunk,
+        })
       }
 
-      console.log(`5 seconds wait before next check for job completion`)
-      await new Promise((resolve) => setTimeout(resolve, 5000))
-    } while (jobStatus < 3)
+      cursor = users[users.length - 1].id
 
-    console.log(jobData)
-
-    const jobUrl = `${stampUrl}/api/queries/${queryId}/results/${jobData.job.query_result_id}.json`
-
-    const jobResponse = await axios.get(jobUrl, headers)
-
-    const rows = jobResponse.data.query_result.data.rows as Row[]
-
-    const chunkSize = 300
-    const chunks = []
-
-    for (let i = 0; i < rows.length; i += chunkSize) {
-      const chunk = rows.slice(i, i + chunkSize)
-      chunks.push(chunk)
+      console.log(`At user index ${cursor}`)
+    } catch (error: any) {
+      console.error(error?.message)
+      console.log(`Continuing at user index: ${cursor}`)
+      await new Promise((res, rej) => setTimeout(res, 10000))
     }
-
-    for (const chunk of chunks) {
-      await prisma.stamp.createMany({
-        data: chunk,
-      })
-    }
-
-    console.log(`At user index ${cursor}`)
   } while ((cursor as number) > 0)
 }
 
