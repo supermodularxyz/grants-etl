b033c517db5c30ffc3ad4495ff0296d7dfb8d0df
Bliss Da Sailor <jedi@wearemaker.studio>
2023-11-13 09:51:53 +0200

    add timestamp to pm2 logs

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -3,26 +3,31 @@ module.exports = {
     {
       name: 'job:1',
       script: 'yarn',
+      time: true,
       args: 'run job:1',
     },
     {
       name: 'job:10',
       script: 'yarn',
+      time: true,
       args: 'run job:10',
     },
     {
       name: 'job:250',
       script: 'yarn',
+      time: true,
       args: 'run job:250',
     },
     {
       name: 'job:424',
       script: 'yarn',
+      time: true,
       args: 'run job:424',
     },
     {
       name: 'job:42161',
       script: 'yarn',
+      time: true,
       args: 'run job:42161',
     },
   ],
