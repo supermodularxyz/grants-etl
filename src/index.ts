de2924509d42832aed6f7ffa9649506147f9ad36
Bliss Da Sailor <jedi@wearemaker.studio>
2023-11-14 13:07:58 +0200

    cleanup

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,11 @@
 import 'dotenv/config'
 import minimist from 'minimist'
-import { main } from './main'
+import { main, disconnect } from './main'
 
 const argv = minimist(process.argv.slice(2), { string: ['roundId'] })
 
-// async function startIndexing() {
-//   // ... you will write your Prisma Client queries here
-//   const chainId = argv.chainId ?? '1' // default to mainnet
-//   const roundId = isAddress(argv.roundId) ? getAddress(argv.roundId) : undefined
-
-//   await initialFetch(chainId)
-
-//   console.log(`Starting programs and rounds indexing`)
-
-//   // manage the programs & rounds first
-//   await manageRounds({ chainId, prisma, roundId })
-
-//   console.log(`Starting round applications indexing`)
-
-//   // manage applications
-//   await manageApplications({ chainId, prisma, roundId })
-
-//   console.log(`Starting projects' votes indexing`)
-
-//   // manage votes
-//   await manageVotes({ chainId, prisma, roundId })
-
-//   console.log(`Starting loading tx metadata for votes`)
-
-//   // manage vote tx metadata
-//   await manageTx({ chainId, prisma })
-
-//   // console.log(`Starting passport indexing`)
-
-//   // await managePassports({ chainId, prisma })
-// }
-
 main({ chainId: argv.chainId, roundId: argv.roundId }).catch(async (e) => {
   console.error(e)
+  await disconnect()
   process.exit(1)
 })
