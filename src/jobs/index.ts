8aa497a5946a5e1eb4aaa71b12eed4c59c09dda1
Jedi Maker <meremezeukah@gmail.com>
2024-02-12 17:12:36 -0600

    cleanup job

--- src/jobs/index.ts
+++ src/jobs/index.ts
@@ -1,21 +1,15 @@
 // import { ToadScheduler, SimpleIntervalJob, AsyncTask } from 'toad-scheduler'
 import schedule from 'node-schedule'
 import minimist from 'minimist'
-import { main, disconnect, indexPassports, indexPoaps, indexArkham, indexStakers } from '../main'
+import { main, disconnect, indexPassports, indexPoaps, indexArkham } from '../main'
 
-const argv = minimist(process.argv.slice(2), { boolean: ['passport', 'paop', 'arkham'] })
+const argv = minimist(process.argv.slice(2), { boolean: ['passport', 'poap', 'arkham'] })
 
 const rule = new schedule.RecurrenceRule()
 rule.hour = 8
 rule.minute = 15
 
-const { chainId, passport, poap, arkham, stakers } = argv
-
-// if (!chainId || chainId.length === 0) {
-//   console.log(`You have to specify a chainId for the cron jobs`)
-
-//   process.exit(1)
-// }
+const { chainId, passport, poap, arkham } = argv
 
 if (chainId) {
   console.log(`Scheduling job for chainId = ${chainId}`)
@@ -70,7 +64,7 @@ if (poap) {
 
 if (arkham) {
   const startIndexing = async () => {
-    console.log(`Starting one-time job for PGN Poap data`)
+    console.log(`Starting one-time job for Arkham data`)
     try {
       await indexArkham()
     } catch (error) {
@@ -82,34 +76,3 @@ if (arkham) {
 
   startIndexing()
 }
-
-if (stakers) {
-  const startIndexing = async () => {
-    console.log(`Starting one-time job for ETH stakers data`)
-    try {
-      await indexStakers()
-    } catch (error) {
-      console.log(error)
-    }
-
-    await disconnect()
-  }
-
-  startIndexing()
-}
-
-// const scheduler = new ToadScheduler()
-
-// const task = new AsyncTask(
-//   'Trial Task',
-//   () => {
-//     return main({ chainId: '250' })
-//   },
-//   (err: Error) => {
-//     /* handle error here */
-//     console.log(err)
-//   }
-// )
-// const job = new SimpleIntervalJob({ hours:  }, task)
-
-// scheduler.addSimpleIntervalJob(job)
