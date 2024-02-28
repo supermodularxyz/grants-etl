36e9919613de39ece3f76054d7f7c6cf064fafa8
Jedi Maker <meremezeukah@gmail.com>
2024-02-28 16:09:43 -0600

    update reformatAddress

--- src/scripts/index.ts
+++ src/scripts/index.ts
@@ -1,17 +1,60 @@
-import { PrismaClient } from '@prisma/client'
+import { PrismaClient, User } from '@prisma/client'
+import Bull from 'bull'
 
 type Props = {
   prisma: PrismaClient
 }
 
 const reformatAddress = async ({ prisma }: Props) => {
-  console.log(`Updating users`)
+  const users = (await prisma.$queryRaw`SELECT * FROM "User" WHERE address != LOWER(address) LIMIT 5000`) as User[]
 
-  const resp = await prisma.$executeRaw`UPDATE "User" SET address = LOWER(address)`
+  const usersQueue = new Bull('reformatAddress')
+  await usersQueue.obliterate({ force: true })
+  await usersQueue.pause()
+  const concurrencyRate = 1000
 
-  console.log({ resp })
+  usersQueue.process(concurrencyRate, async (job: Bull.Job<{ id: number; address: string }>) => {
+    const { data: user } = job
 
-  console.log(`Updated user fields`)
+    await prisma.user.update({
+      where: {
+        id: user.id,
+      },
+      data: {
+        address: user.address.toLowerCase(),
+      },
+    })
+
+    console.log(`Updated ${user.address}: ${await usersQueue.count()} left`)
+
+    return Promise.resolve(true)
+  })
+
+  usersQueue.on('drained', async () => {
+    const newUsers = (await prisma.$queryRaw`SELECT * FROM "User" WHERE address != LOWER(address) LIMIT 5000`) as User[]
+
+    if (newUsers.length === 0) {
+      console.log(`Updated user fields`)
+      usersQueue.close()
+    } else {
+      await usersQueue.pause()
+      for (const user of newUsers) {
+        usersQueue.add(user)
+      }
+      await usersQueue.resume()
+    }
+  })
+
+  usersQueue.on('failed', async (job) => {
+    return await job.retry()
+  })
+
+  // Add voters to queue
+  console.log(`Adding users to queue`)
+  for (const user of users) {
+    usersQueue.add(user)
+  }
+  await usersQueue.resume()
 }
 
 export default reformatAddress
